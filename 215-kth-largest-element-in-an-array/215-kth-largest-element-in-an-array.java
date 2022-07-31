9816292c966292adc0c14820264951ac4a4dfcbd
Ayushman <53306550+chetak123@users.noreply.github.com>
2022-07-31 10:07:22 +0530

    Time: 74 ms (9.74%), Space: 77.5 MB (5.07%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+     
+        PriorityQueue<Integer> pq =new PriorityQueue<>();
+        
+        //creating a min heap of k elements 
+        // here the root element will be the kth largest element 
+        for (int i=0;i<k;i++){
+            pq.add(nums[i]);
+            
+        }
+        for (int i=k;i<nums.length;i++){
+            if (pq.peek()<nums[i]){ // to check if the first element of queue 
+                // is less than arr element, min heap root element
+                
+                pq.poll(); // remove first element from queue, or root from min 
+                // heap
+                pq.add(nums[i]);
+                //it then internally maintains the min heap
+                
+            }
+            
+        }
+        return pq.poll();
+        
+    }
+}
