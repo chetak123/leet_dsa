e50acbac52b0bbcf63e2ca3c50051dacc07b685c
Ayushman <53306550+chetak123@users.noreply.github.com>
2022-11-01 16:34:44 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+        static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+  
+    /* This function takes last element as pivot, places
+       the pivot element at its correct position in sorted
+       array, and places all smaller (smaller than pivot)
+       to left of pivot and all greater elements to right
+       of pivot */
+    static int partition(int[] arr, int low, int high)
+    {
+  
+        // pivot
+        int pivot = arr[high];
+  
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+  
+        for (int j = low; j <= high - 1; j++) {
+  
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot) {
+  
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+  
+    /* The main function that implements QuickSort
+              arr[] --> Array to be sorted,
+              low --> Starting index,
+              high --> Ending index
+     */
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high) {
+  
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+  
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+  
+    // Function to print an array
+    static void printArray(int[] arr, int size)
+    {
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+  
+        System.out.println();
+    }
+
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+
+        // Return K'th element in
+        // the sorted array
+        int n = arr.length;
+  
+        quickSort(arr, 0, n - 1);
+        return arr[k-1];
+
+
+        
+    } 
+}
